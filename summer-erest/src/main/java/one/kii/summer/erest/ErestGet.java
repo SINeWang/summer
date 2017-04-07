4cb0a6b079030d61aa2c7a9335a3e57289617b52
SINeWang <im.sinewang@gmail.com>
2017-04-07 16:52:04 +0800

    improve ErestGet and ErestPost

--- summer-erest/src/main/java/one/kii/summer/erest/ErestGet.java
+++ summer-erest/src/main/java/one/kii/summer/erest/ErestGet.java
@@ -2,37 +2,33 @@ package one.kii.summer.erest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.UUID;
+
 /**
- * Created by WangYanJiong on 02/04/2017.
+ * Created by WangYanJiong on 4/7/17.
  */
 public class ErestGet {
 
     private static Logger logger = LoggerFactory.getLogger(ErestGet.class);
-    
-    public <T> T forAny(String urlTemplate, Class<T> klass, Object... uriVariables) {
-        logger.debug("forAny:{}, return:{}", urlTemplate, klass.getSimpleName());
+
+    public <T> T doGet(String urlTemplate, HttpHeaders headers, Class<T> klass, Object... uriVariables) {
+        logger.debug("doGet:{}, return:{}", urlTemplate, klass.getSimpleName());
+
+        headers.set("X-SUMMER-RequestId", UUID.randomUUID().toString());
 
         RestTemplate restTemplate = new RestTemplate();
 
-        Class type = new Type(klass).getType();
+        HttpEntity request = new HttpEntity<>(headers);
 
-        ResponseEntity<T> response = restTemplate.getForEntity(urlTemplate, klass, uriVariables);
+        ResponseEntity<T> response = restTemplate.exchange(urlTemplate, HttpMethod.GET, request, klass, uriVariables);
         return response.getBody();
+
     }
 
-    private class Type {
-
-        private Class type;
-
-        private Type(Class type) {
-            this.type = type;
-        }
-
-        Class getType() {
-            return type;
-        }
-    }
 }
---
+++ summer-erest/src/main/java/one/kii/summer/erest/ErestPostFormUrlEncoded.java
@@ -0,0 +1,49 @@
+package one.kii.summer.erest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+/**
+ * Created by WangYanJiong on 4/7/17.
+ */
+public class ErestPostFormUrlEncoded {
+
+    private static Logger logger = LoggerFactory.getLogger(ErestPostFormUrlEncoded.class);
+
+    public <T> T doPost(String urlTemplate, HttpHeaders headers, MultiValueMap<String, String> bodyMap, Class<T> klass, Object... uriVariables) {
+        logger.debug("forAny:{}, return:{}", urlTemplate, klass.getSimpleName());
+
+        headers.set("X-SUMMER-RequestId", UUID.randomUUID().toString());
+        headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(bodyMap, headers);
+
+
+        ResponseEntity<T> response = restTemplate.exchange(urlTemplate, HttpMethod.POST, request, klass, uriVariables);
+        return response.getBody();
+
+    }
+
+}
+
+//        Class type = new Type(klass).getType();
+//    private class Type {
+//
+//        private Class type;
+//
+//        private Type(Class type) {
+//            this.type = type;
+//        }
+//
+//        Class getType() {
+//            return type;
+//        }
+//    }
