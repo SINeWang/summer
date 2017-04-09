f4cac0663e8ec36822617b1e0c7d23aaa15da810
SINeWang <im.sinewang@gmail.com>
2017-04-09 22:17:29 +0800

    add conflict & not modified response

--- summer-erest/src/main/java/one/kii/summer/erest/ErestPostForm.java
+++ summer-erest/src/main/java/one/kii/summer/erest/ErestPostForm.java
@@ -3,9 +3,14 @@ package one.kii.summer.erest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -29,7 +34,43 @@ public class ErestPostForm {
 
         ResponseEntity<T> response = restTemplate.exchange(urlTemplate, HttpMethod.POST, request, klass, uriVariables);
         return response.getBody();
-
     }
 
+
+    public <T> T doPost(String urlTemplate, HttpHeaders headers, Object form, Class<T> klass, Object... uriVariables) {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+
+        Field[] fields = form.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                Object value = field.get(form);
+                map.put(field.getName(), toList(value));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return doPost(urlTemplate, headers, map, klass, uriVariables);
+    }
+
+    private List<String> toList(Object object) {
+        if (object == null) {
+            return Collections.emptyList();
+        }
+        List<String> list = new ArrayList<>();
+        if (!object.getClass().isArray()) {
+            list.add(object.toString());
+            return list;
+        } else {
+            Object[] objects = (Object[]) object;
+            for (Object value : objects) {
+                if (value != null) {
+                    list.add(value.toString());
+                }
+            }
+            return list;
+        }
+    }
 }
--- summer-erest/src/main/java/one/kii/summer/erest/Response.java
+++ summer-erest/src/main/java/one/kii/summer/erest/Response.java
@@ -39,4 +39,20 @@ public class Response {
         return new ResponseEntity<>(resource, headers, HttpStatus.NOT_FOUND);
     }
 
+    public static <T> ResponseEntity<T> conflict(String requestId, T object) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-SUMMER-Time", new Date().toString());
+        headers.set("X-SUMMER-RequestId", requestId);
+        headers.set("X-SUMMER-ResponseId", UUID.randomUUID().toString());
+        return new ResponseEntity<>(object, headers, HttpStatus.CONFLICT);
+    }
+
+    public static <T> ResponseEntity<T> notModified(String requestId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-SUMMER-Time", new Date().toString());
+        headers.set("X-SUMMER-RequestId", requestId);
+        headers.set("X-SUMMER-ResponseId", UUID.randomUUID().toString());
+        return new ResponseEntity<>(headers, HttpStatus.NOT_MODIFIED);
+    }
+
 }
