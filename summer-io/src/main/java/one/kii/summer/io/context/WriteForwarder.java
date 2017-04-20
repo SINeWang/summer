668aea2efb4ee10ad5380d4dc1e5370f9e7dfc0a
SINeWang <im.sinewang@gmail.com>
2017-04-20 21:19:28 +0800

    refact WriteForwarder

--- summer-io/src/main/java/one/kii/summer/io/context/WriteForwarder.java
+++ summer-io/src/main/java/one/kii/summer/io/context/WriteForwarder.java
@@ -14,12 +14,7 @@ public class WriteForwarder extends ActionForwarder {
         if (targetOwnerId == null) {
             throw new BadRequest(OwnerId.FIELD_NAME);
         }
-        WriteContext writeContext = new WriteContext(
-                UUID.randomUUID().toString(),
-                targetOwnerId,
-                context.getOperatorId());
-        writeContext.referId = context.getProcessId();
-        return writeContext;
+        return buildWriteContext(context, targetOwnerId);
 
     }
 
@@ -27,12 +22,7 @@ public class WriteForwarder extends ActionForwarder {
         if (targetOwnerId == null) {
             throw new BadRequest(OwnerId.FIELD_NAME);
         }
-        WriteContext writeContext = new WriteContext(
-                UUID.randomUUID().toString(),
-                targetOwnerId,
-                context.getOperatorId());
-        writeContext.referId = context.getProcessId();
-        return writeContext;
+        return buildWriteContext(context, targetOwnerId);
 
     }
 
@@ -40,18 +30,50 @@ public class WriteForwarder extends ActionForwarder {
         if (targetOwnerId == null) {
             throw new BadRequest(OwnerId.FIELD_NAME);
         }
-        WriteContext writeContext = new WriteContext(
-                UUID.randomUUID().toString(),
-                targetOwnerId,
-                context.getVisitorId());
-        writeContext.referId = context.getProcessId();
-        return writeContext;
+        return buildWriteContext(context, targetOwnerId);
     }
 
     public static WriteContext from(ReadAuthorizationContext context, String targetOwnerId) throws BadRequest {
         if (targetOwnerId == null) {
             throw new BadRequest(OwnerId.FIELD_NAME);
         }
+        return buildWriteContext(context, targetOwnerId);
+    }
+
+    public static WriteContext from(ReadAuthorizationContext context, Object target) throws BadRequest {
+        String ownerId = findOwnerIdFromObject(target);
+        return buildWriteContext(context, ownerId);
+    }
+
+
+
+    public static WriteContext from(ReadContext context, Object target) throws BadRequest {
+        String ownerId = findOwnerIdFromObject(target);
+        return buildWriteContext(context, ownerId);
+    }
+
+    public static WriteContext from(WriteContext context, Object target) throws BadRequest {
+        String ownerId = findOwnerIdFromObject(target);
+        return buildWriteContext(context, ownerId);
+    }
+
+
+    public static WriteContext from(WriteAuthorizationContext context, Object target) throws BadRequest {
+        String ownerId = findOwnerIdFromObject(target);
+        return buildWriteContext(context, ownerId);
+
+    }
+
+    private static WriteContext buildWriteContext(ReadContext context, String ownerId) {
+        WriteContext writeContext = new WriteContext(
+                UUID.randomUUID().toString(),
+                ownerId,
+                context.getVisitorId());
+        writeContext.referId = context.getProcessId();
+        return writeContext;
+    }
+
+    private static WriteContext buildWriteContext(ReadAuthorizationContext context, String targetOwnerId) {
         WriteContext writeContext = new WriteContext(
                 UUID.randomUUID().toString(),
                 targetOwnerId,
@@ -60,44 +82,21 @@ public class WriteForwarder extends ActionForwarder {
         return writeContext;
     }
 
-    public static WriteContext from(ReadAuthorizationContext context, Object target) throws BadRequest {
-        String ownerId = findOwnerIdFromObject(target);
+    private static WriteContext buildWriteContext(WriteContext context, String targetOwnerId) {
         WriteContext writeContext = new WriteContext(
                 UUID.randomUUID().toString(),
-                ownerId,
-                context.getVisitorId());
-        writeContext.referId = context.getProcessId();
-        return writeContext;
-    }
-
-    public static WriteContext from(ReadContext context, Object target) throws BadRequest {
-        String ownerId = findOwnerIdFromObject(target);
-        WriteContext writeContext = new WriteContext(
-                UUID.randomUUID().toString(),
-                ownerId,
-                context.getVisitorId());
-        writeContext.referId = context.getProcessId();
-        return writeContext;
-    }
-
-    public static WriteContext from(WriteContext context, Object target) throws BadRequest {
-        String ownerId = findOwnerIdFromObject(target);
-        WriteContext writeContext = new WriteContext(
-                UUID.randomUUID().toString(),
-                ownerId,
+                targetOwnerId,
                 context.getOperatorId());
         writeContext.referId = context.getProcessId();
         return writeContext;
     }
 
-    public static WriteContext from(WriteAuthorizationContext context, Object target) throws BadRequest {
-        String ownerId = findOwnerIdFromObject(target);
+    private static WriteContext buildWriteContext(WriteAuthorizationContext context, String targetOwnerId) {
         WriteContext writeContext = new WriteContext(
                 UUID.randomUUID().toString(),
-                ownerId,
+                targetOwnerId,
                 context.getOperatorId());
         writeContext.referId = context.getProcessId();
         return writeContext;
-
     }
 }
