ee26a6d68691004abd5e75f9cbfd3ff503607c0e
SINeWang <im.sinewang@gmail.com>
2017-04-20 16:35:21 +0800

    support MustHave annotations

--- summer-io/src/main/java/one/kii/summer/io/validator/Must.java
+++ summer-io/src/main/java/one/kii/summer/io/validator/Must.java
@@ -1,7 +1,9 @@
 package one.kii.summer.io.validator;
 
+import one.kii.summer.io.annotations.MustHave;
 import one.kii.summer.io.exception.BadRequest;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,26 +37,51 @@ public class Must {
                 badFields.add(fieldName);
             }
             if (field != null) {
-                field.setAccessible(true);
-                Object value = null;
-                try {
-                    value = field.get(object);
-                } catch (IllegalAccessException e) {
-                    badFields.add(fieldName);
-                }
-                if (value == null) {
-                    badFields.add(fieldName);
-                }
-                if (value instanceof String) {
-                    String str = (String) value;
-                    if (str.trim().length() == 0) {
-                        badFields.add(fieldName);
-                    }
-                }
+                checkValueNotEmpty(object, badFields, field);
             }
         }
         if (badFields.size() > 0) {
             throw new BadRequest(badFields.toArray(new String[0]));
+        }
+    }
+
+
+    public static void have(Object object) throws BadRequest {
+        if (object == null) {
+            throw new NullPointerException();
+        }
+        Class type = object.getClass();
+        Field[] fields = type.getDeclaredFields();
+        List<String> badFields = new ArrayList<>();
+        for (Field field : fields) {
+            Annotation annotation = field.getAnnotation(MustHave.class);
+            if (annotation == null) {
+                continue;
+            }
+            checkValueNotEmpty(object, badFields, field);
+        }
+        if (badFields.size() > 0) {
+            throw new BadRequest(badFields.toArray(new String[0]));
+        }
+    }
+
+
+    private static void checkValueNotEmpty(Object object, List<String> badFields, Field field) {
+        field.setAccessible(true);
+        Object value = null;
+        try {
+            value = field.get(object);
+        } catch (IllegalAccessException e) {
+            badFields.add(field.getName());
+        }
+        if (value == null) {
+            badFields.add(field.getName());
+        }
+        if (value instanceof String) {
+            String str = (String) value;
+            if (str.trim().length() == 0) {
+                badFields.add(field.getName());
+            }
         }
     }
 }
