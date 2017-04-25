4e759f7def5e03a5dd142947633bc332fcccbfe6
SINeWang <im.sinewang@gmail.com>
2017-04-25 16:57:14 +0800

    add LowerHyphen annotation

---
+++ summer-beans/src/main/java/one/kii/summer/beans/annotations/LowerHyphen.java
@@ -0,0 +1,15 @@
+package one.kii.summer.beans.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by WangYanJiong on 25/04/2017.
+ */
+
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LowerHyphen {
+}
