c892a075082b50998764f810567f2deb0b2ecdc2
SINeWang <im.sinewang@gmail.com>
2017-05-05 16:44:27 +0800

    try to support enum copy

--- summer-beans/src/main/java/one/kii/summer/beans/utils/DataTools.java
+++ summer-beans/src/main/java/one/kii/summer/beans/utils/DataTools.java
@@ -2,6 +2,7 @@ package one.kii.summer.beans.utils;
 
 import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +21,40 @@ public class DataTools {
             e.printStackTrace();
         }
         BeanUtils.copyProperties(src, instance);
+
+//        Field[] fields = klass.getDeclaredFields();
+//        for (Field targetField : fields) {
+//            if (!targetField.getType().isEnum()) {
+//                continue;
+//            }
+//            Object targetValue = null;
+//            try {
+//                targetValue = targetField.get(instance);
+//            } catch (IllegalAccessException e) {
+//                continue;
+//            }
+//            if (targetValue == null) {
+//                Field srcField;
+//                try {
+//                    srcField = src.getClass().getDeclaredField(targetField.getName());
+//                } catch (NoSuchFieldException e) {
+//                    continue;
+//                }
+//                Object srcValue;
+//                try {
+//                    srcField.setAccessible(true);
+//                    srcValue = srcField.get(src);
+//                } catch (IllegalAccessException e) {
+//                    continue;
+//                }
+//                if (srcValue != null) {
+//                    targetField.setAccessible(true);
+//                    targetField.set(instance, Enum.valueOf((Enum)targetField.getType(), String.valueOf(srcValue)));
+//                }
+//            }
+//
+//        }
+
         return instance;
     }
 
