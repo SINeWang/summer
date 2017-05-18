a4e992543233dfde5471bc57b95792316abbd7d2
SINeWang <im.sinewang@gmail.com>
2017-05-18 16:53:59 +0800

    support map as source type

--- summer-beans/src/main/java/one/kii/summer/beans/utils/MagicCopy.java
+++ summer-beans/src/main/java/one/kii/summer/beans/utils/MagicCopy.java
@@ -5,6 +5,7 @@ import org.springframework.beans.BeanUtils;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 /**
  * Created by WangYanJiong on 02/04/2017.
@@ -35,68 +36,82 @@ public class MagicCopy {
     private static <T> void fillMissingFields(T target, Object... sources) {
         Field[] targetFields = target.getClass().getDeclaredFields();
         for (Field targetField : targetFields) {
-            Object value;
+            Object targetValue;
             try {
                 targetField.setAccessible(true);
-                value = targetField.get(target);
+                targetValue = targetField.get(target);
             } catch (IllegalAccessException e) {
                 //ignore
                 continue;
             }
-            if (value != null) {
+            if (targetValue != null) {
                 continue;
             }
             for (int i = 1; i < sources.length; i++) {
-                Field sourceField = null;
-                boolean noSuchField = false;
-                try {
-                    sourceField = sources[i].getClass().getDeclaredField(targetField.getName());
-                } catch (NoSuchFieldException e) {
-                    noSuchField = true;
-                }
-                if (noSuchField) {
-                    String fieldName = targetField.getName();
-                    char first = Character.toUpperCase(fieldName.charAt(0));
-                    String getMethodName = "get" + first + fieldName.substring(1);
-                    Method method;
-                    try {
-                        method = sources[i].getClass().getMethod(getMethodName);
-                    } catch (NoSuchMethodException e) {
-                        continue;
-                    }
-                    try {
-                        value = method.invoke(sources[i]);
-                    } catch (IllegalAccessException e) {
-                        //ignore
-                    } catch (InvocationTargetException e) {
-                        //ignore
-                    }
-                    if (value != null) {
-                        try {
-                            targetField.set(target, value);
-                        } catch (IllegalAccessException e) {
-                            // ignore
+                Object sourceValue = null;
+                if (sources[i] instanceof Map) {
+                    Map map = (Map) sources[i];
+                    sourceValue = map.get(targetField.getName());
+                    if (sourceValue != null) {
+                        if (targetField.getType().equals(sourceValue.getClass())) {
+                            setValue(target, targetField, sourceValue);
+                        } else {
+                            targetValue = BasicCopy.from(targetField.getType(), sourceValue);
+                            setValue(target, targetField, targetValue);
                         }
                     }
-                    break;
                 } else {
-                    if (sourceField.getType().equals(targetField.getType())) {
-                        sourceField.setAccessible(true);
+                    Field sourceField = null;
+                    boolean noSuchField = false;
+                    try {
+                        sourceField = sources[i].getClass().getDeclaredField(targetField.getName());
+                    } catch (NoSuchFieldException e) {
+                        noSuchField = true;
+                    }
+                    if (noSuchField) {
+                        String fieldName = targetField.getName();
+                        char first = Character.toUpperCase(fieldName.charAt(0));
+                        String getMethodName = "get" + first + fieldName.substring(1);
+                        Method method;
                         try {
-                            value = sourceField.get(sources[i]);
+                            method = sources[i].getClass().getMethod(getMethodName);
+                        } catch (NoSuchMethodException e) {
+                            continue;
+                        }
+                        try {
+                            sourceValue = method.invoke(sources[i]);
                         } catch (IllegalAccessException e) {
                             //ignore
+                        } catch (InvocationTargetException e) {
+                            //ignore
                         }
-                        if (value != null) {
-                            try {
-                                targetField.set(target, value);
-                            } catch (IllegalAccessException e) {
-                                // ignore
-                            }
-                        }
+                        setValue(target, targetField, sourceValue);
                         break;
+                    } else {
+                        if (sourceField.getType().equals(targetField.getType())) {
+                            sourceField.setAccessible(true);
+                            try {
+                                sourceValue = sourceField.get(sources[i]);
+                            } catch (IllegalAccessException e) {
+                                //ignore
+                            }
+                            setValue(target, targetField, sourceValue);
+                            break;
+                        }
                     }
                 }
+
+
+            }
+        }
+    }
+
+    private static <T> void setValue(T target, Field targetField, Object value) {
+        if (value != null) {
+            try {
+                targetField.set(target, value);
+            } catch (IllegalAccessException e) {
+                // ignore
             }
         }
     }
