d2b542108f78f61f79f4d7ec1fe55b1628cc8ae5
SINeWang <im.sinewang@gmail.com>
2017-06-03 20:16:30 +0800

    improve test case

--- summer-asdf/src/test/java/one/kii/summer/asdf/xi/TestCommitApiCaller.java
+++ summer-asdf/src/test/java/one/kii/summer/asdf/xi/TestCommitApiCaller.java
@@ -11,25 +11,95 @@ import org.springframework.util.Assert;
  */
 public class TestCommitApiCaller {
 
+
+    static String[] keys = new String[]{};
+    WriteContext context = new WriteContext("requestId", "ownerId", "operatorId");
+    Object form = new Object();
+
     @Test
-    public void test() {
-
-        CommitApi api = new TestCommitApi();
-
-        WriteContext context = new WriteContext("requestId", "ownerId", "operatorId");
-
-        Object form = new Object();
-
+    public void testOK() {
+        CommitApi api = new TestOK();
         Object resp = CommitApiCaller.call(api, context, form);
         Assert.isInstanceOf(ResponseEntity.class, resp);
 
     }
 
-    public static class TestCommitApi implements CommitApi {
 
+    @Test
+    public void testBadRequest() {
+        CommitApi api = new TestBadRequest();
+        Object resp = CommitApiCaller.call(api, context, form);
+        Assert.isInstanceOf(ResponseEntity.class, resp);
+    }
+
+    @Test
+    public void testConflict() {
+        CommitApi api = new TestConflict();
+        Object resp = CommitApiCaller.call(api, context, form);
+        Assert.isInstanceOf(ResponseEntity.class, resp);
+    }
+
+    @Test
+    public void testForbidden() {
+        CommitApi api = new TestForbidden();
+        Object resp = CommitApiCaller.call(api, context, form);
+        Assert.isInstanceOf(ResponseEntity.class, resp);
+    }
+
+    @Test
+    public void testNotFound() {
+        CommitApi api = new TestNotFound();
+        Object resp = CommitApiCaller.call(api, context, form);
+        Assert.isInstanceOf(ResponseEntity.class, resp);
+    }
+
+    @Test
+    public void testPanic() {
+        CommitApi api = new TestPanic();
+        Object resp = CommitApiCaller.call(api, context, form);
+        Assert.isInstanceOf(ResponseEntity.class, resp);
+    }
+
+
+    public static class TestOK implements CommitApi {
         @Override
         public Object commit(WriteContext context, Object form) throws BadRequest, Conflict, Forbidden, NotFound, Panic {
             return new Object();
+        }
+    }
+
+    public static class TestNotFound implements CommitApi {
+        @Override
+        public Object commit(WriteContext context, Object form) throws BadRequest, Conflict, Forbidden, NotFound, Panic {
+            throw new NotFound(keys);
+        }
+    }
+
+    public static class TestConflict implements CommitApi {
+        @Override
+        public Object commit(WriteContext context, Object form) throws BadRequest, Conflict, Forbidden, NotFound, Panic {
+            throw new Conflict(keys);
+        }
+    }
+
+    public static class TestBadRequest implements CommitApi {
+        @Override
+        public Object commit(WriteContext context, Object form) throws BadRequest, Conflict, Forbidden, NotFound, Panic {
+            throw new BadRequest(keys);
+        }
+    }
+
+    public static class TestForbidden implements CommitApi {
+        @Override
+        public Object commit(WriteContext context, Object form) throws BadRequest, Conflict, Forbidden, NotFound, Panic {
+            throw new Forbidden(keys);
+        }
+    }
+
+    public static class TestPanic implements CommitApi {
+        @Override
+        public Object commit(WriteContext context, Object form) throws BadRequest, Conflict, Forbidden, NotFound, Panic {
+            throw new Panic();
         }
     }
 }
