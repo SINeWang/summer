9e1efed8d946b05d2c16d2340e3ea56966c48c3b
SINeWang <im.sinewang@gmail.com>
2017-06-05 08:27:41 +0800

    add search api

---
+++ summer-asdf/src/main/java/one/kii/summer/asdf/xi/SearchApi.java
@@ -0,0 +1,14 @@
+package one.kii.summer.asdf.xi;
+
+import one.kii.summer.io.context.ReadContext;
+import one.kii.summer.io.exception.BadRequest;
+import one.kii.summer.io.exception.Panic;
+
+/**
+ * Created by WangYanJiong on 05/06/2017.
+ */
+public interface SearchApi<R, C extends ReadContext, F> {
+
+    R visit(C context, F form) throws BadRequest, Panic;
+
+}
