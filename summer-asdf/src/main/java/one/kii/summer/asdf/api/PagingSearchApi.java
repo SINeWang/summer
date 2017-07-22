f7c37039ed27121970624d699353f3afe11725a7
SINeWang <im.sinewang@gmail.com>
2017-07-22 20:28:12 +0800

    add PagingSearchApi

---
+++ summer-asdf/src/main/java/one/kii/summer/asdf/api/PagingSearchApi.java
@@ -0,0 +1,40 @@
+package one.kii.summer.asdf.api;
+
+import lombok.Data;
+import one.kii.summer.io.annotations.MayHave;
+import one.kii.summer.io.context.ReadContext;
+import one.kii.summer.io.exception.BadRequest;
+import one.kii.summer.io.exception.Panic;
+
+import java.util.List;
+
+/**
+ * Created by WangYanJiong on 05/06/2017.
+ */
+public interface PagingSearchApi<R, C extends ReadContext, F extends PagingSearchApi.Form> {
+
+    Receipt<R> search(C context, F form) throws BadRequest, Panic;
+
+
+    @Data
+    class Receipt<R> {
+        List<R> data;
+        Integer total;
+    }
+
+    @Data
+    class Form {
+
+        @MayHave
+        String beginPage;
+
+        @MayHave
+        String endPage;
+
+        @MayHave
+        String order;
+
+        @MayHave
+        String orderBy;
+    }
+}
