85cd1540346a2e5842f18e06ce74763658fe994d
SINeWang <im.sinewang@gmail.com>
2017-08-06 09:28:10 +0800

    update search controller

--- summer-io/src/main/java/one/kii/summer/io/receiver/SearchController.java
+++ summer-io/src/main/java/one/kii/summer/io/receiver/SearchController.java
@@ -1,8 +1,6 @@
 package one.kii.summer.io.receiver;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-public abstract class SearchController<T> extends ReadController {
+public abstract class SearchController extends ReadController {
 
 
     public final static String REQUEST_KEY_QUERY = "q";
@@ -14,10 +12,6 @@ public abstract class SearchController<T> extends ReadController {
     public final static String DEFAULT_PAGE = "0";
 
     public final static String DEFAULT_SIZE = "30";
-
-    @Autowired
-    private T api;
-
 
 
 }
