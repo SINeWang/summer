f5ca02007807c3597970185229a5db83bf13975a
SINeWang <im.sinewang@gmail.com>
2017-12-09 20:31:28 +0800

    improve simple paging search api

--- summer-io/src/main/java/one/kii/summer/asdf/api/SimplePagingSearchApi.java
+++ summer-io/src/main/java/one/kii/summer/asdf/api/SimplePagingSearchApi.java
@@ -1,34 +1,9 @@
 package one.kii.summer.asdf.api;
 
-import lombok.Data;
-import one.kii.summer.io.annotations.MayHave;
 import one.kii.summer.io.context.ReadContext;
-
-import java.util.List;
 
 
 public interface SimplePagingSearchApi<R, F> extends PagingSearchApi<R, ReadContext, F> {
 
 
-    @Data
-    class Receipt<R> {
-        List<R> data;
-        Integer total;
-    }
-
-    @Data
-    class Paging {
-
-        @MayHave
-        Integer begin;
-
-        @MayHave
-        Integer size;
-
-        @MayHave
-        String sort;
-
-        @MayHave
-        String orderBy;
-    }
 }
