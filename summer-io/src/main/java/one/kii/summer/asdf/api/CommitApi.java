a4cb9698ad25a48be69564d0e4814ed76950e32c
SINeWang <im.sinewang@gmail.com>
2017-12-10 20:02:18 +0800

    add validator

--- summer-io/src/main/java/one/kii/summer/asdf/api/CommitApi.java
+++ summer-io/src/main/java/one/kii/summer/asdf/api/CommitApi.java
@@ -3,10 +3,12 @@ package one.kii.summer.asdf.api;
 import one.kii.summer.io.context.WriteContext;
 import one.kii.summer.io.exception.*;
 
+import javax.validation.Valid;
+
 /**
  * Created by WangYanJiong on 03/06/2017.
  */
 public interface CommitApi<R, C extends WriteContext, F> {
 
-    R commit(C context, F form) throws BadRequest, Conflict, Forbidden, NotFound, Panic;
+    R commit(C context, @Valid F form) throws BadRequest, Conflict, Forbidden, NotFound, Panic;
 }
