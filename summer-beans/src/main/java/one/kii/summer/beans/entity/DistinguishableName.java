c2d98bc7d1ad4e0c0e88a5d312a1f9348b8a11d9
SINeWang <im.sinewang@gmail.com>
2017-12-10 13:56:35 +0800

    add constructors

--- summer-beans/src/main/java/one/kii/summer/beans/entity/DistinguishableName.java
+++ summer-beans/src/main/java/one/kii/summer/beans/entity/DistinguishableName.java
@@ -1,8 +1,12 @@
 package one.kii.summer.beans.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DistinguishableName {
 
     private String name;
