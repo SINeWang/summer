4c3c376cca7f30536a1ae1a7e7b0e8fe1846b03b
SINeWang <im.sinewang@gmail.com>
2017-12-10 19:38:21 +0800

    rename

--- summer-beans/src/main/java/one/kii/summer/beans/annotations/AcceptableValues.java
+++ summer-beans/src/main/java/one/kii/summer/beans/annotations/AcceptableValues.java
@@ -5,7 +5,7 @@ package one.kii.summer.beans.annotations;
  * https://stackoverflow.com/a/21070806/1206735
  */
 
-import one.kii.summer.beans.validator.EnumValidator;
+import one.kii.summer.beans.validator.AcceptableValuesValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
@@ -14,12 +14,12 @@ import javax.validation.constraints.NotNull;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = EnumValidator.class)
+@Constraint(validatedBy = AcceptableValuesValidator.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 @NotNull(message = "value cannot be null")
 @ReportAsSingleViolation
-public @interface EnumReference {
+public @interface AcceptableValues {
 
     Class<? extends Enum<?>> range();
 
--- summer-beans/src/main/java/one/kii/summer/beans/validator/AcceptableValuesValidator.java
+++ summer-beans/src/main/java/one/kii/summer/beans/validator/AcceptableValuesValidator.java
@@ -1,6 +1,6 @@
 package one.kii.summer.beans.validator;
 
-import one.kii.summer.beans.annotations.EnumReference;
+import one.kii.summer.beans.annotations.AcceptableValues;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -12,7 +12,7 @@ import java.util.List;
  * https://stackoverflow.com/a/21070806/1206735
  */
 
-public class EnumValidator implements ConstraintValidator<EnumReference, String> {
+public class AcceptableValuesValidator implements ConstraintValidator<AcceptableValues, String> {
 
     List<String> values = null;
 
@@ -25,11 +25,11 @@ public class EnumValidator implements ConstraintValidator<EnumReference, String>
     }
 
     @Override
-    public void initialize(EnumReference constraintAnnotation) {
+    public void initialize(AcceptableValues constraintAnnotation) {
         values = new ArrayList<>();
-        Class<? extends Enum<?>> klass = constraintAnnotation.klass();
+        Class<? extends Enum<?>> range = constraintAnnotation.range();
 
-        @SuppressWarnings("rawtypes") Enum[] enumValArr = klass.getEnumConstants();
+        @SuppressWarnings("rawtypes") Enum[] enumValArr = range.getEnumConstants();
 
         for (@SuppressWarnings("rawtypes") Enum enumVal : enumValArr) {
             values.add(enumVal.toString().toUpperCase());
