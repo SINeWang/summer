d8d5ddbb7a47bbde00e15f0f0b9856a3d81a8b56
SINeWang <im.sinewang@gmail.com>
2017-12-18 21:42:54 +0800

    add alias annotation

---
+++ summer-beans/src/main/java/one/kii/summer/beans/annotations/Alias.java
@@ -0,0 +1,14 @@
+package one.kii.summer.beans.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Alias {
+
+    String as();
+
+}
