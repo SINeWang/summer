bf08c2c20e5f2bd671d8dad4e23e557b7a4f37ff
SINeWang <im.sinewang@gmail.com>
2017-12-30 15:14:40 +0800

    fix endtime bug

--- summer-datetime/src/main/java/one/kii/summer/datetime/Constant.java
+++ summer-datetime/src/main/java/one/kii/summer/datetime/Constant.java
@@ -2,5 +2,5 @@ package one.kii.summer.datetime;
 
 public class Constant {
 
-    public final static String SQL_END_TIME = "'2100-01:01 00:00:00'";
+    public final static String SQL_END_TIME = "'2100-01-01 00:00:00'";
 }
