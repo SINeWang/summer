00b95a3efa4c6c032308265644257f767c929bbf
SINeWang <im.sinewang@gmail.com>
2018-01-01 15:34:34 +0800

    add group-name

---
+++ summer-beans/src/main/java/one/kii/summer/beans/entity/GroupName.java
@@ -0,0 +1,14 @@
+package one.kii.summer.beans.entity;
+
+import lombok.Data;
+
+@Data
+public class GroupName {
+
+    String group;
+    String name;
+
+    String operatorId;
+    String beginTime;
+    String endTime;
+}
