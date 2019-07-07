c0020c9ae402a39d9e9bc4e9f6d409098fc13990
SINeWang <im.sinewang@gmail.com>
2019-07-07 16:21:24 +0800

    fix class casting issue

--- summer-io/src/main/java/one/kii/summer/io/utils/MultiValueForm.java
+++ summer-io/src/main/java/one/kii/summer/io/utils/MultiValueForm.java
@@ -2,9 +2,11 @@ package one.kii.summer.io.utils;
 
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Objects;
 
 
@@ -23,23 +25,30 @@ public class MultiValueForm {
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
 
         Class type = object.getClass();
-        Field[] fields = type.getDeclaredFields();
-        for (Field field : fields) {
+        ReflectionUtils.doWithFields(type, field -> {
             field.setAccessible(true);
+            if (field.isSynthetic()) {
+                return;
+            }
             Object value;
             try {
                 value = field.get(object);
             } catch (IllegalAccessException e) {
-                continue;
+                return;
             }
-            Class vType = value.getClass().getComponentType();
-            if (vType == null) {
-                map.set(field.getName(), String.valueOf(value));
-            } else {
+            if (value == null) {
+                return;
+            }
+            if (Collection.class.isAssignableFrom(value.getClass())) {
+                ((Collection<Object>) value).stream().filter(Objects::nonNull)
+                        .forEachOrdered(v -> map.add(field.getName(), String.valueOf(v)));
+            } else if (value.getClass().isArray()) {
                 Arrays.stream((Object[]) value).filter(Objects::nonNull)
                         .forEachOrdered(v -> map.add(field.getName(), String.valueOf(v)));
+            } else {
+                map.set(field.getName(), String.valueOf(value));
             }
-        }
+        });
         return map;
     }
 }
